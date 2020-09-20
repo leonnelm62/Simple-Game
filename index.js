b076289d9d5bbf7e01fca297e16a2e5e2189df6c
leonnelm62 <leonnelm62@gmail.com>
2020-09-20 22:01:22 +0100

    structure

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -90,7 +92,46 @@ const stepThroughCell = (row, column) => {
         } else if(direction === 'down'){
             horizontals[row][column] = true;
         }
+        stepThroughCell(nextRow, nextColumn);
     }
 };
 
 stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open === true) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+        columnIndex * unitLength + unitLength / 2,
+        rowIndex * unitLength + unitLength,
+        unitLength,
+        10,
+        {
+            isStatic: true
+        }
+        );
+        World.add(world, wall);
+  });
+});
+
+verticals.forEach((row, rowIndex) =>{
+    row.forEach((open, columnIndex) =>{
+        if(open){
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            10,
+            unitLength,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall);
+    });
+});
